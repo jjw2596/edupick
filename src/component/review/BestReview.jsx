8dab76acd2ec4a3ade2319d13bce223a236e799f
jjw2596 <jjw2596@naver.com>
2024-03-18 17:44:18 +0900

    240318 best ing2

--- src/component/review/BestReview.jsx
+++ src/component/review/BestReview.jsx
@@ -11,6 +11,8 @@ import best from "../../assets/best.png";
 import searchbtn from "../../assets/searchbtn.png";
 import reviewPhoto from "../../assets/reviewPhoto.png";
 import reviewPhotoPlus from "../../assets/reviewPhotoPlus.png";
+import aiAnalysis from "../../assets/aiAnalysis.png";
+import reviewPhoto2 from "../../assets/reviewPhoto2.png";
 
 function BestReview() {
   return (
@@ -20,13 +22,27 @@ function BestReview() {
       <div className="logoSlider">
         <div className="logoWrapper">
           <img src={best} alt="best" className="serviceLogo selectedLogo" />
-          <img src={milkt} alt="milkt" className="serviceLogo" />
-          <img src={smartall} alt="smartall" className="serviceLogo" />
-          <img src={elihigh} alt="elihigh" className="serviceLogo" />
-          <img src={onlyone} alt="only1" className="serviceLogo" />
-          <img src={homelearn} alt="homelearn" className="serviceLogo" />
+          <img src={milkt} alt="milkt" className="serviceLogo unselectLogo" />
+          <img
+            src={smartall}
+            alt="smartall"
+            className="serviceLogo unselectLogo"
+          />
+          <img
+            src={elihigh}
+            alt="elihigh"
+            className="serviceLogo unselectLogo"
+          />
+          <img src={onlyone} alt="only1" className="serviceLogo unselectLogo" />
+          <img
+            src={homelearn}
+            alt="homelearn"
+            className="serviceLogo unselectLogo"
+          />
         </div>
       </div>
+
+      <div className="boardTitle">Best Review</div>
 
       <div>
         <div className="searchContainer">
@@ -52,6 +68,7 @@ function BestReview() {
           </div>
         </div>
         <div className="recommend">
+          <span>추천 검색어 | </span>
           <span>영어 | </span>
           <span>수학 | </span>
           <span>자기주도 | </span>
@@ -80,6 +97,74 @@ function BestReview() {
           </div>
           <div className="photoItem">
             <img src={reviewPhotoPlus} alt="" width="100%" height="100%" />
+          </div>
+        </div>
+      </div>
+
+      {/* 리뷰 게시글 영역 */}
+      <div className="reviewContainer">
+        {/* 전체리뷰 txt */}
+        <div className="reviewTitleContainer">
+          <div className="reviewTitle">
+            <span>베스트 리뷰</span>
+            <span className="pointTypo"> 100개</span>
+          </div>
+        </div>
+
+        {/* 게시글 */}
+        <div className="reviewWrapper">
+          <div className="reviewContentWrapper">
+            <div className="reviewLeft">
+              <img src={elihigh} alt="" width="80%" height="80%" />
+            </div>
+            <div className="reviewRight">
+              <div className="reviewTop">
+                <span>아이가 너무 좋아하고 컨텐츠도 풍부해요!</span>
+              </div>
+              <div className="reviewBottom">
+                <div>
+                  <span>작성자 | tnals****</span>
+                  <span>&nbsp; &nbsp; 2024.03.01</span>
+                </div>
+                <div>
+                  <span>좋아요 123</span>
+                  <span>&nbsp;&nbsp;신고</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="aiAnalysis">
+            <img src={aiAnalysis} alt="" width="100%" height="100%" />
+          </div>
+
+          <div className="reviewContent">
+            <p>
+              다른거보다 아이들이 꾸준히 책상에 앉는 습관이 꾀 길러졌다고
+              봅니다.두달동안 긴 시간은 아니더라도짧은시간 동안 재미난 수업도
+              하고바로 피드백이 있는거 같아서 아이들도 좋아하고 학부모 입장에서
+              그때그때 일 과제 마무리 했는지 안했는지 확인도...
+            </p>
+          </div>
+
+          <div className="reviewPhotoContainer">
+            <div className="reviewPhoto">
+              <img src={reviewPhoto2} alt="" width="100%" height="100%" />
+            </div>
+            <div className="reviewPhoto">
+              <img src={reviewPhoto2} alt="" width="100%" height="100%" />
+            </div>
+            <div className="reviewPhoto">
+              <img src={reviewPhoto2} alt="" width="100%" height="100%" />
+            </div>
+            <div className="reviewPhoto">
+              <img src={reviewPhoto2} alt="" width="100%" height="100%" />
+            </div>
+          </div>
+
+          <div className="reviewWrapperBottom">
+            <div>댓글 2개</div>
+            <div></div>
           </div>
         </div>
       </div>
